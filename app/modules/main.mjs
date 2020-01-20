919e84da82b337699390338079aa40303db0b954
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2020-01-19 20:42:34 -0500

    Add dynamic event handlers

--- app/modules/main.mjs
+++ app/modules/main.mjs
@@ -1,9 +1,16 @@
-document.getElementById("btnSoundA").addEventListener("click", function(event) {
-  const audioEl = document.getElementById("audSoundA");
-  audioEl.play();
-});
+/* main.mjs */
 
-document.getElementById("btnSoundB").addEventListener("click", function(event) {
-    const audioEl = document.getElementById("audSoundB");
-    audioEl.play();
-  });
+const init = function() {
+    const soundElements = document.getElementsByClassName("sound");
+    for (let soundEl of soundElements) {
+        const buttonEl = soundEl.getElementsByTagName("button")[0];
+        const audioEl = soundEl.getElementsByTagName("audio")[0];
+        buttonEl.addEventListener("click", event => {
+            audioEl.play();
+        });
+    }
+};
+
+window.addEventListener("DOMContentLoaded", function(event) {
+    init();
+});
