d72244ebdfd3ed952204c53bc9d550de8ef7f0b7
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2020-02-04 11:26:30 -0500

    Add detailed audio play and pause

--- app/modules/main.mjs
+++ app/modules/main.mjs
@@ -1,13 +1,46 @@
 /* main.mjs */
 
+const setAudioPlayStatus = (audioEl, status) => {
+    audioEl.dataset.isPlaying = status;
+};
+
+const setupAudioPlay = function(soundEl) {
+    const buttonEl = soundEl.getElementsByTagName("button")[0];
+    const audioEl = soundEl.getElementsByTagName("audio")[0];
+
+    audioEl.addEventListener("play", event => {
+        setAudioPlayStatus(event.target, true);
+    });
+
+    audioEl.addEventListener("pause", event => {
+        setAudioPlayStatus(event.target, false);
+    });
+
+    audioEl.addEventListener("ended", event => {
+        setAudioPlayStatus(event.target, false);
+    });
+
+    buttonEl.addEventListener("click", event => {
+        if (audioEl.dataset.isPlaying === "true") {
+            // Loops are paused,
+            // while non-looping are reset and played again.
+            if (audioEl.loop) {
+                audioEl.pause();
+            } else {
+                audioEl.pause();
+                audioEl.currentTime = 0;
+                audioEl.play();
+            }
+        } else {
+            audioEl.play();
+        }
+    });
+};
+
 const init = function() {
     const soundElements = document.getElementsByClassName("sound");
     for (let soundEl of soundElements) {
-        const buttonEl = soundEl.getElementsByTagName("button")[0];
-        const audioEl = soundEl.getElementsByTagName("audio")[0];
-        buttonEl.addEventListener("click", event => {
-            audioEl.play();
-        });
+        setupAudioPlay(soundEl);
     }
 };
 
