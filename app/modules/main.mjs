64d6906f582da71fce2dadf7c4ec003c32322698
Owen <5332788+coldspire@users.noreply.github.com>
2020-02-04 11:33:16 -0500

    Basic audio (#14)
    * Add stub sounds and working audio
    
    * Add dynamic event handlers
    
    * Add detailed audio play and pause

--- app/modules/main.mjs
+++ app/modules/main.mjs
@@ -1,1 +1,49 @@
-import * as audio from './audio.mjs';
+/* main.mjs */
+
+const setAudioPlayStatus = (audioEl, status) => {
+    audioEl.dataset.isPlaying = status;
+};
+
+const setupAudioPlay = function(soundEl) {
+    const buttonEl = soundEl.getElementsByTagName("button")[0];
+    const audioEl = soundEl.getElementsByTagName("audio")[0];
+
+    audioEl.addEventListener("play", event => {
+        setAudioPlayStatus(event.target, true);
+    });
+
+    audioEl.addEventListener("pause", event => {
+        setAudioPlayStatus(event.target, false);
+    });
+
+    audioEl.addEventListener("ended", event => {
+        setAudioPlayStatus(event.target, false);
+    });
+
+    buttonEl.addEventListener("click", event => {
+        if (audioEl.dataset.isPlaying === "true") {
+            // Loops are paused,
+            // while non-looping are reset and played again.
+            if (audioEl.loop) {
+                audioEl.pause();
+            } else {
+                audioEl.pause();
+                audioEl.currentTime = 0;
+                audioEl.play();
+            }
+        } else {
+            audioEl.play();
+        }
+    });
+};
+
+const init = function() {
+    const soundElements = document.getElementsByClassName("sound");
+    for (let soundEl of soundElements) {
+        setupAudioPlay(soundEl);
+    }
+};
+
+window.addEventListener("DOMContentLoaded", function(event) {
+    init();
+});
